39bff598c15be4347fe6f1b0b9232135f0ce9338
WebMohit20 <2003mohitkhemani@gmail.com>
2024-11-24 13:22:22 +0000

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
+
 const App = () => {
+  let [sum,setSum] = useState(0);
   return (
     <div>
+      <input type="number" onChange={(e)=>setSum(sum+ +(e.target.value))}/>
+      <h1>{sum}</h1>
         {/* Do not remove the main div */}
     </div>
   )
